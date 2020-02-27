5cf738db35b61edd3507b5342d5c79f2fc633bdc
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-26 22:01:42 -0600

    added alert when attempts have run out

--- js/aboutme.js
+++ js/aboutme.js
@@ -6,25 +6,26 @@ var attempts = 3;
 // var restaurants = ['La Vecina', 'Marquee', 'Pullmans', 'Farmers and Distillers', 'Crabcake Factory'];
 
 function promptQuiz(){
-var question6 = prompt('How many places have I worked?');
-var question6Num = Number(question6);
-
-while (attempts > 0){
-
+    var question6Num = Number(prompt('How many places have I worked?'));
     var guessNum = 5;
 
-    if(question6Num === guessNum){
+    while (attempts > 0){
+        if(question6Num === guessNum){
 
-        alert('Correct!');
-        attempts = 0;
+            alert('Correct!');
+            attempts = 0;
 
+        }
+        if(question6Num > guessNum){
+            question6Num = Number(prompt('Too high. Try again.'));
+            attempts--;
+        }
+        if(question6Num < guessNum){
+            question6Num = Number(prompt('Too low. Try again.'));
+            attempts--;
+        }
     }
-    if(question6Num > guessNum){
-        question6Num = prompt('Too high. Try again.');
-        attempts--;
+    if(attempts === 0){
+        alert('You ran out of attempts. The answer was ' + guessNum + '.');
     }
-    if(question6Num < guessNum){
-        question6Num = prompt('Too low. Try again.');
-        attempts--;
-    }
-}}
+}
