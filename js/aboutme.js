5246b194884ac574e058ac783d569791af99e7ee
cnickels21 <cnickels_21@yahoo.com>
2020-02-27 15:15:16 -0600

    added question 7 function and score alert

--- js/aboutme.js
+++ js/aboutme.js
@@ -2,10 +2,12 @@
 
 var attempts = 3;
 var userPoints = 0;
-// var attemptsExt = 0;
+var attemptsExt = 0;
 
 function promptQuiz() {
     question6();
+    question7();
+    alertScore();
 }
 
 function question6() {
@@ -35,7 +37,29 @@ function question6() {
 }
 
 function question7() {
-    var restaurants = ['La Vecina', 'Marquee', 'Pullmans', 'Farmers and Distillers', 'Crabcake Factory'];
-    
+    var restaurants = ['La Vecina', 'Marquee', 'Pullmans', 'Farmers and Distillers', 'Crabcake Factory', 'Clinton Street Social Club', 'Rapid Creek Cidery', 'Moniker 86 Social Club', 'Exotic India', 'Brix Wine Bar'];
+    while (attemptsExt < 6) {
+        var favRes = prompt('What is one of my favorite restaurants?');
+        for (var i = 0; i < restaurants.length; i++) {
+            console.log(i);
+            if (favRes === restaurants[i]) {
+                alert('You got the right answer.');
+                attemptsExt = 6;
+                userPoints++;
+                console.log(userPoints);
+                break;
+            }
+        }
+        if (favRes !== restaurants[i]) {
+            alert('Sorry guess again.');
+            attemptsExt++;
+        }
+    }
+    if (attemptsExt === 6 && favRes !== restaurants[i]) {
+        alert('Sorry, you ran out of attempts.');
+    }
+}
 
+function alertScore() {
+    alert('Congratulations! You have ' + userPoints + ' points.');
 }
