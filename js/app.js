c28008a78690dfc54cbe9da36bca068e7286b5ef
Stacey Teltser <stacey.teltser@gmail.com>
2020-03-01 20:57:16 -0600

    wrapped first 5 questions in functions

--- js/app.js
+++ js/app.js
@@ -9,39 +9,55 @@ var name = prompt('Hello there! What is your name?');
 
 
 function promptMe(){
+  question1prompt();
+  question2prompt();
+  question3prompt();
+  question4prompt();
+  question5prompt();
+}
+
+function question1prompt(){
   var question1 = prompt('Do I have 4 children?').toLowerCase();
   while (question1 !== 'no') {
     question1 = prompt('Try again. Do I have 4 children?').toLowerCase();
   } 
+}
 
+function question2prompt(){
   var question2 = prompt('Correct! Next question... Did I move to Cedar Rapids in 2011?').toLowerCase();
   while (question2 !== 'yes') {
     question2 = prompt('Try again. Did I move to Cedar Rapids in 2011?').toLowerCase();
   }
+}
 
+function question3prompt(){
   var question3 = prompt('You are rocking this quiz! Next up, did I receive my degree in Economics?').toLowerCase();
   while (question3 !== 'no') {
     question3 = prompt('Try again. Did I receive my degree in Economics?').toLowerCase();
   }
+}
 
+function question4prompt(){
   var question4 = prompt('You got another one right! Did I work as a sign language interpreter after graduating college?').toLowerCase();
   while (question4 !== 'yes') {
     question4 = prompt('Try again. Did I work as a sign language interpreter after graduating college?').toLowerCase();
   }
+}
 
+function question5prompt(){
   var question5 = prompt('Last one! Is one of my favorite shows The Office?').toLowerCase();
   while (question5 !== 'yes') {
     question5 = prompt('Try again. Is one of my favorite shows The Office?').toLowerCase();
   }
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
 
 greeting();
 
